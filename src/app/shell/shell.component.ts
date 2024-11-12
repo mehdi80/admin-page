79b7ea3c6c8a815dfe4dcfa2b08bd4d4e1e41fc7
mehdi80 <mk2954751@gmail.com>
2024-11-13 02:29:43 +0330

    create header component and styling

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../components/header/header.component";
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
+
+    colors: {
+
+      green: {
+        100: '#C2FFC7',
+        200:'#9EDF9C',
+        300:'#62825D',
+        400:'#526E48',
+      },
+    },
     extend: {},
   },
   plugins: [],
