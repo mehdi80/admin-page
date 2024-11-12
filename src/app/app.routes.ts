96bbb34cf04a9e9ff80d86bfca8c2b46b2be7f25
mehdi80 <mk2954751@gmail.com>
2024-11-12 19:21:29 +0330

    create shell component and moved admin component in to components folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {AdminComponent} from "./components/admin/admin.component";
+import {ShellComponent} from "./shell/shell.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:ShellComponent ,
+    children:[
+
+    ]},
+];
