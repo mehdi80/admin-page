9e1db08520a924a9df400b02d2bea26391f6a50b
mehdi80 <mk2954751@gmail.com>
2024-11-13 14:49:38 +0330

    create and styling: login and register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import {AdminComponent} from "./components/admin/admin.component";
 import {ShellComponent} from "./shell/shell.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 export const routes: Routes = [
   {
     path:'',
     component:ShellComponent ,
     children:[
-
+      {path:'login' ,component:LoginComponent},
+      {path:'register' ,component:RegisterComponent},
     ]},
+
 ];
--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../components/header/header.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-shell',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    RouterOutlet
   ],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss'
