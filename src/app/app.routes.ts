3cb5d5c20af849d2457cf154f792bcc205c18717
mehdi80 <mk2954751@gmail.com>
2024-11-13 17:11:33 +0330

    create user list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {AdminComponent} from "./components/admin/admin.component";
 import {ShellComponent} from "./shell/shell.component";
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {UserListComponent} from "./components/user-list/user-list.component";
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
     children:[
       {path:'login' ,component:LoginComponent},
       {path:'register' ,component:RegisterComponent},
+      {path:'user-list' ,component:UserListComponent},
     ]},
 
 ];
