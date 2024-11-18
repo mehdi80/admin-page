f6654be78d38db3f46a1730d1dd8feb6f78bce02
mehdi80 <mk2954751@gmail.com>
2024-11-18 16:09:53 +0330

    added auth router

---
+++ src/app/modules/auth.route.ts
@@ -0,0 +1,16 @@
+import {Routes} from "@angular/router";
+
+export const authRoutes:Routes = [
+  {path:'',
+  loadComponent:()=> import('../shared/components/auth/auth.component').then(auth=>auth.AuthComponent)
+  },
+
+  {
+    path:'login',
+    loadComponent:()=> import('../shared/components/login/login.component').then(login=>login.LoginComponent)
+  },
+  {
+    path:'register',
+    loadComponent:() =>import('../shared/components/register/register.component').then(register=>register.RegisterComponent)
+  }
+]
